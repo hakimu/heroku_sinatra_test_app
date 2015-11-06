f50e7e45d87cb501518480fcb4229628f6f002be
Hakimu Jackson <hakimu@newrelic.com>
2015-11-06 09:20:11 -0800

    Updated config/puma.rb to remove ActiveRecord stuff

--- config/puma.rb
+++ config/puma.rb
@@ -8,8 +8,9 @@ rackup      DefaultRackup
 port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
-on_worker_boot do
-  # Worker specific setup for Rails 4.1+
-  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-  ActiveRecord::Base.establish_connection
-end
+#Commented the below due to getting errors
+# on_worker_boot do
+#   # Worker specific setup for Rails 4.1+
+#   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+#   ActiveRecord::Base.establish_connection
+# end
