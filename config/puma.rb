210e187758c248a6103c10ddfc46d85d576d7c72
Hakimu Jackson <hakimu@newrelic.com>
2016-06-15 13:22:20 -0700

    Updated puma_worker_config settings

--- config/puma.rb
+++ config/puma.rb
@@ -1,4 +1,8 @@
-require 'puma_worker_killer'
+before_fork do 
+  require 'puma_worker_killer'
+  PumaWorkerKiller.enable_rolling_restart(30)
+
+end
 
 workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = Integer(ENV['MAX_THREADS'] || 5)
@@ -10,7 +14,7 @@ rackup      DefaultRackup
 port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
-PumaWorkerKiller.enable_rolling_restart(30)
+# PumaWorkerKiller.enable_rolling_restart(30)
 
 #Commented the below due to getting errors
 # on_worker_boot do
