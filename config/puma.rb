4e8a1b4697e4996018a1b95be4b0a35a729603d4
Hakimu Jackson <hakimu@newrelic.com>
2016-06-15 14:28:32 -0700

    Updated to remove puma_worker_killer gem

--- config/puma.rb
+++ config/puma.rb
@@ -1,9 +1,3 @@
-before_fork do 
-  require 'puma_worker_killer'
-  PumaWorkerKiller.enable_rolling_restart(30)
-
-end
-
 workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = Integer(ENV['MAX_THREADS'] || 5)
 threads threads_count, threads_count
@@ -14,7 +8,7 @@ rackup      DefaultRackup
 port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
-# PumaWorkerKiller.enable_rolling_restart(30)
+
 
 #Commented the below due to getting errors
 # on_worker_boot do
